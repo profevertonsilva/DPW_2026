c1c7bda8bc65eac8ea07cdb77fc109e7f5ff87f6
lbonatoc <luccabonatoc@gmail.com>
2026-04-02 20:42:13 -0300

    Adição dos Atributos ao modelo da Clinica

---
+++ App/Model/ClinicaModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Model;
+
+class ClinicaModel {
+    private $cln_id;
+    private $cln_cnpj;
+    private $cln_nome;
+    private $cln_cep;
+    private $cln_estado;
+    private $cln_cidade;
+    private $cln_bairro;
+    private $cln_logradouro;
+    private $cln_cidade;
+    private $cln_numero;
+    private $cln_complemento;
+    private $cln_tel1;
+    private $cln_tel2;
+
+
+    public __get($nome) {
+        return $this->$nome;
+    }
+
+    public __set($nome, $valor) {
+        $this->$nome = $valor;
+    }
+}
