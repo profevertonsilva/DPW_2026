2a54c3d2661fac0c122c258380666f0bdb3f4f7e
lbonatoc <luccabonatoc@gmail.com>
2026-04-03 13:01:53 -0300

    Adição dos atributos e metodos da classe SolicitacaoAdocao no Model

---
+++ App/Model/SolicitacaoAdocaoModel.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Model;
+
+class SolicitacaoAdocaoModel {
+
+    private $solAdc_id;
+    private $solAdc_data;
+    private $solAdc_status;
+    private $solAdc_motivo;
+    private $fk_adotante_id;
+    private $fk_animal_id;
+
+    public __get($nome) {
+        return $this->$nome;
+    }
+
+    public __set($nome, $valor) {
+        $this->$nome = $valor;
+    }
+
+}
