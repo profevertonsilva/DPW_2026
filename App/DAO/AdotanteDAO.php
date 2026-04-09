d0bc2fefcd6f745f78f396c5eea2bd122b036fab
lbonatoc <luccabonatoc@gmail.com>
2026-04-09 20:53:22 -0300

    Adição da classe AdotanteDAO ao DAO

---
+++ App/DAO/AdotanteDAO.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DAO;
+
+
+use App\DAO;
+use App\Model\AdotanteModel;
+use FW\Controller\FuncoesGlobais;
+
+class AdotanteDAO extends DAO {
+    public  function inserir($obj) {
+    
+    };
+    public  function excluir($obj) {
+
+    };
+    public  function alterar($obj) {
+
+    };
+    public  function buscarPorId($obj) {
+
+    };
+    public  function listar() {
+
+    };
+
+}
