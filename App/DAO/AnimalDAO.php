8cbde64039ffd623d45e595e39079b1f209be262
Marcus Rito <rito.marcus@hotmail.com>
2026-04-24 14:10:35 -0300

    Ajustes no AnimalDAO
    Implementado a função buscarPorId, alterar, excluir e buscarPorLogado.

--- App/DAO/AnimalDAO.php
+++ App/DAO/AnimalDAO.php
@@ -1,4 +1,7 @@
 <?php
+/*
+ * @Author marcus rito
+ */
 
 namespace App\DAO;
 
@@ -13,9 +16,8 @@ class AnimalDAO extends DAO
     public function inserir($obj)
     {
         try {
-
             $anl_nome = $obj->__get('anl_nome');
-            $anl_dn = $obj->__get('anl_dn');
+            $anl_dn   = $obj->__get('anl_dn');
             $anl_sexo = $obj->__get('anl_sexo');
 
             $sql = "INSERT INTO animal (
@@ -27,9 +29,10 @@ class AnimalDAO extends DAO
                         :anl_dn,
                         :anl_sexo
                     )";
+
             $stmt = $this->getConn()->prepare($sql);
             $stmt->bindValue(':anl_nome', $anl_nome);
-            $stmt->bindValue(':anl_dn', $anl_dn);
+            $stmt->bindValue(':anl_dn',   $anl_dn);
             $stmt->bindValue(':anl_sexo', $anl_sexo);
             $stmt->execute();
         } catch (\PDOException $ex) {
@@ -40,11 +43,10 @@ class AnimalDAO extends DAO
 
     public function listar()
     {
-
         try {
             $animais = array();
-            $sql = "SELECT a.*
-                    FROM animal a";
+
+            $sql = "SELECT * FROM animal";
 
             $stmt = $this->getConn()->prepare($sql);
             $stmt->execute();
@@ -52,11 +54,11 @@ class AnimalDAO extends DAO
 
             foreach ($result as $row) {
                 $animalModel = new AnimalModel();
-
                 $global = new FuncoesGlobais();
                 $global->popularModel($animalModel, $row);
                 array_push($animais, $animalModel);
             }
+
             return $animais;
         } catch (\PDOException $ex) {
             header('Location:/error103');
@@ -64,8 +66,97 @@ class AnimalDAO extends DAO
         }
     }
 
-    public function excluir($obj) {}
-    public function alterar($obj) {}
-    public function buscarPorId($id) {}
-    public function buscarPorLogado($id) {}
+    public function buscarPorId($id)
+    {
+        try {
+            $sql = "SELECT * FROM animal WHERE id = :id";
+
+            $stmt = $this->getConn()->prepare($sql);
+            $stmt->bindValue(':id', $id, \PDO::PARAM_INT);
+            $stmt->execute();
+
+            $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+            if ($result) {
+                $animalModel = new AnimalModel();
+                $global = new FuncoesGlobais();
+                $global->popularModel($animalModel, $result);
+                return $animalModel;
+            }
+
+            return null;
+        } catch (\PDOException $ex) {
+            header('Location:/error103');
+            die();
+        }
+    }
+
+    public function alterar($obj)
+    {
+        try {
+            $anl_id   = $obj->__get('anl_id');
+            $anl_nome = $obj->__get('anl_nome');
+            $anl_dn   = $obj->__get('anl_dn');
+            $anl_sexo = $obj->__get('anl_sexo');
+
+            $sql = "UPDATE animal SET
+                        nome            = :anl_nome,
+                        data_nascimento = :anl_dn,
+                        sexo            = :anl_sexo
+                    WHERE
+                        id = :anl_id";
+
+            $stmt = $this->getConn()->prepare($sql);
+            $stmt->bindValue(':anl_id',   $anl_id,   \PDO::PARAM_INT);
+            $stmt->bindValue(':anl_nome', $anl_nome);
+            $stmt->bindValue(':anl_dn',   $anl_dn);
+            $stmt->bindValue(':anl_sexo', $anl_sexo);
+            $stmt->execute();
+        } catch (\PDOException $ex) {
+            header('Location:/error103');
+            die();
+        }
+    }
+
+    public function excluir($obj)
+    {
+        try {
+            $anl_id = $obj->__get('anl_id');
+
+            $sql = "DELETE FROM animal WHERE id = :anl_id";
+
+            $stmt = $this->getConn()->prepare($sql);
+            $stmt->bindValue(':anl_id', $anl_id, \PDO::PARAM_INT);
+            $stmt->execute();
+        } catch (\PDOException $ex) {
+            header('Location:/error103');
+            die();
+        }
+    }
+
+    public function buscarPorLogado($id)
+    {
+        try {
+            $sql = "SELECT * FROM animal WHERE id = :id";
+
+            $stmt = $this->getConn()->prepare($sql);
+            $stmt->bindValue(':id', $id, \PDO::PARAM_INT);
+            $stmt->execute();
+
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+            $animais = array();
+            foreach ($result as $row) {
+                $animalModel = new AnimalModel();
+                $global = new FuncoesGlobais();
+                $global->popularModel($animalModel, $row);
+                array_push($animais, $animalModel);
+            }
+
+            return $animais;
+        } catch (\PDOException $ex) {
+            header('Location:/error103');
+            die();
+        }
+    }
 }
