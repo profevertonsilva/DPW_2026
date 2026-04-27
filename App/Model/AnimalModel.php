b820e5c7266ff33f376a1298d1c84f14514dcc7d
Marcus Rito <rito.marcus@hotmail.com>
2026-04-27 08:21:50 -0300

    Adição dos campos faltantes no AnimalModel

--- App/Model/AnimalModel.php
+++ App/Model/AnimalModel.php
@@ -1,19 +1,36 @@
 <?php
+/*
+ * @Author marcus rito
+ */
+
 
 namespace App\Model;
 
-class AnimalModel {
+class AnimalModel
+{
 
     private $ani_id;
     private $ani_nome;
     private $ani_sexo;
     private $ani_data_nascimento;
 
-    public function __get($nome) {
+    // Inserção dos novos campos - 27/04/2026
+    private $ani_cor;
+    private $ani_porte;
+    private $ani_status;
+    private $ani_castrado;
+    private $ani_foto;
+    private $ani_descricao;
+    private $ani_fk_especie_id;
+
+
+    public function __get($nome)
+    {
         return $this->$nome;
     }
 
-    public function __set($nome, $valor) {
+    public function __set($nome, $valor)
+    {
         $this->$nome = $valor;
     }
 }
