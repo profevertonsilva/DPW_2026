d1ca4fc733abd3a327b3ca96bb5209f7d0450169
lbonatoc <luccabonatoc@gmail.com>
2026-05-03 00:39:25 -0300

    Adição de um atributo ao model do Adotante, fk_login_log_id

--- App/Model/AdotanteModel.php
+++ App/Model/AdotanteModel.php
@@ -18,6 +18,7 @@ class AdotanteModel {
     private $adt_tel1;
     private $adt_tel2;
     private $adt_status;
+    private $fk_login_log_id;
 
     public function __set($nome, $valor) {
         $this->$nome = $valor;
