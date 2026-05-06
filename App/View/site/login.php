a8566930c4d1100dad529e0792c7079d679cddae
Derek Cazarin <134968713+DerekxD82@users.noreply.github.com>
2026-05-06 20:17:54 -0300

    Adição de um link que direciona pra landing page

--- App/View/site/login.php
+++ App/View/site/login.php
@@ -1,4 +1,5 @@
 <div class="container-fluid">
     <h1>HTML DA PÁGINA LOGIN</h1>
     <p>Esta página será implementada em breve.</p>
+    <a href="http://localhost:8000/App/View/site/landing_page.php">Direciona pra Landing Page</a>
 </div>
