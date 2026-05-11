17762961901afd778d48058345b9ea1177868efc
lbonatoc <luccabonatoc@gmail.com>
2026-05-11 02:03:55 -0300

    Correções das verificações de erro da view de login

--- App/View/site/login.php
+++ App/View/site/login.php
@@ -18,7 +18,7 @@
                     </div>
                 <?php elseif ($_GET['erro'] == 3): ?>
                     <div class="alert alert-danger" role="alert">
-                        Você precisa estar logado para acessar essa página.
+                        Você precisa não possue login no sistema.
                     </div>
                 <?php endif; ?>
             <?php endif; ?>
