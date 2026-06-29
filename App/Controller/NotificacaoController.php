9164aaf90f2d3a6558b81b0b81dc36ff3d85c60e
WillianOliveira <williandeoliveiramarquesolivei@gmail.com>
2026-06-28 23:18:13 -0300

    Criação do Controller das Notificações

---
+++ App/Controller/NotificacaoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use FW\Controller\Action;
+use App\DAO\NotificacaoDAO;
+
+class NotificacaoController extends Action
+{
+    public function listar()
+    {
+        $this->validaAutenticacao();
+
+        $dao = new NotificacaoDAO();
+
+        $this->getView()->notificacoes =
+            $dao->listarPorUsuario($_SESSION['id']);
+
+        $this->getView()->title = "Notificações";
+        $this->getView()->title_pagina = "Notificações";
+
+        $this->render('../dashboard/notificacao_listar', 'dashboard');
+    }
+
+    public function marcarComoLida()
+    {
+        $this->validaAutenticacao();
+
+        $id = $_POST['id'] ?? null;
+
+        if ($id) {
+
+            $dao = new NotificacaoDAO();
+            $dao->marcarComoLida($id);
+
+        }
+
+        header('Location: /dashboard/notificacao/listar');
+        die();
+    }
+
+    public function contarNaoLidas()
+    {
+        $this->validaAutenticacao();
+
+        $dao = new NotificacaoDAO();
+
+        echo $dao->contarNaoLidas($_SESSION['id']);
+    }
+}
